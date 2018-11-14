2538510d88f055f8fbb807000a93348529beeda5
Loorem <loorem@tlu.ee>
2018-11-14 19:47:01 +0200

    enemy

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,16 +9,28 @@ public class Bullet : MonoBehaviour {
 	public float moveSpeed = 7.0f;
 	
 	private float timePassed = 0.0f;
+    private float damage = 1.0f;
+    private GameObject triggerEnemy;
 	
 	//functions
 	
-	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 		transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
 		timePassed+=Time.deltaTime;
 		if(timePassed>=5)
-		Destroy(this.gameObject);
-
-	}
+        {
+		    Destroy(this.gameObject);
+        }
+    }
+        void OnTriggerEnter(Collider other)
+        {
+        if(other.tag == "Enemy")
+{
+        triggerEnemy = other.gameObject;
+        triggerEnemy.GetComponent<Enemy>().health -= damage;
+        Destroy(this.gameObject);
+}
+}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,20 +17,31 @@ public class Player : MonoBehaviour {
         if(Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-			//transform.getchild(0) LookAt();
+			if(Input.GetKeyDown(KeyCode.W))
+                transform.GetChild(0).rotation = Quaternion.Euler(0,0,0);
+            //transform.getchild(0) LookAt();
 			
         }// Player movement
         if(Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            if(Input.GetKeyDown(KeyCode.A))
+                transform.GetChild(0).rotation = Quaternion.Euler(0,-90,0);
+            //transform.getchild(0) LookAt();
         }// Player movement
         if(Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+            if(Input.GetKeyDown(KeyCode.S))
+                transform.GetChild(0).rotation = Quaternion.Euler(0,180,0);
+            //transform.getchild(0) LookAt();
         }// Player movement
         if(Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.D))
+                transform.GetChild(0).rotation = Quaternion.Euler(0, 90, 0);
+            //transform.getchild(0) LookAt();
         }
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
